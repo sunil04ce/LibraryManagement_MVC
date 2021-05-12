1f154bb66ed5cb4bb8068dc8c3640b320a4bde13
Sunil Parmar <sunil04@gmail.com>
2021-05-12 17:57:02 +0530

    added entity database model login screen with validation

---
+++ LibraryManagement_MVC/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using LibraryManagement_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                using(LibraryManagementContext db = new LibraryManagementContext())
+                {
+                    var obj = db.Users.Where(x => x.Email.Equals(user.Email) && x.Password.Equals(user.Password)).FirstOrDefault();
+                    if(obj != null)
+                    {
+                        Session["UserId"] = obj.Id.ToString();
+                        Session["UserName"] = obj.Name.ToString();
+                        //Session["UserEmail"] = obj.Email;
+                        return RedirectToAction("DashBoard");
+                    } 
+                    else
+                    {
+                        ModelState.AddModelError("", "The email or password provided is incorrect.");
+                    }
+                }
+            }
+
+            return View(user);
+        }
+
+        public ActionResult DashBoard()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
