69cc2181304e693a8e94087c0f74bc06215931b9
Sunil Parmar <sunil04@gmail.com>
2021-05-18 18:24:04 +0530

    added areas, changed menu titles

---
+++ LibraryManagement_MVC/Areas/Book/BookAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace LibraryManagement_MVC.Areas.Book
+{
+    public class BookAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Book";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Book_default",
+                "Book/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
